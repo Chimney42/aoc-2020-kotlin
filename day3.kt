51d0d384c868a4ac410de012877130768316b96a
Lian <draggili@gmail.com>
2020-12-03 21:04:56 +0100

    day3 part2

--- day3.kt
+++ day3.kt
@@ -19,17 +19,26 @@ fun createMap(): List<List<String>> {
 
 fun main() {
   val map = createMap()
-  val slopeX = 3
-  val slopeY = 1
+  val slopes = listOf(
+    mapOf("x" to 1, "y" to 1), 
+    mapOf("x" to 3, "y" to 1), 
+    mapOf("x" to 5, "y" to 1), 
+    mapOf("x" to 7, "y" to 1), 
+    mapOf("x" to 1, "y" to 2)
+  )
+  var totalTreeCount = 1
 
-  var x = 0
-  var y = 0
-  var treeCount = 0
-  while (y < map.size) {
-    if (hasTree(x, y, map)) { treeCount++ }
-    x += slopeX
-    y += slopeY
+  for (slope in slopes) {
+    var x = 0
+    var y = 0
+    var treeCount = 0
+    while (y < map.size) {
+      if (hasTree(x, y, map)) { treeCount++ }
+      x += slope.get("x")!!
+      y += slope.get("y")!!
+    }
+    println(treeCount)
+    totalTreeCount *= treeCount
   }
-
-  println(treeCount)
+  println(totalTreeCount)
 }
