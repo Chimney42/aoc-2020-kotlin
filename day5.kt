7cf172bd988167e4e29e1eb696fad0824f7ff203
Lian <draggili@gmail.com>
2020-12-06 12:54:43 +0100

    day5 part2

--- day5.kt
+++ day5.kt
@@ -31,13 +31,20 @@ fun calculateSeatId(row: Int, column: Int): Int {
 
 fun main() {
   val inputStream: InputStream = File("data/day5-input.txt").inputStream()
-  var highestSeatId = 0
 
+  val allSeatsIds = IntRange(0, 127 * 8 + 7)
+  val occupiedSeatIds = mutableListOf<Int>()
   inputStream.bufferedReader().forEachLine {
     val row = findRow(it.slice(IntRange(0, 6)))
     val column = findColumn(it.slice(IntRange(7, 9)))
     val seatId = calculateSeatId(row, column)
-    highestSeatId = if (seatId >= highestSeatId) seatId else highestSeatId
+    occupiedSeatIds.add(seatId)
   }
-  println(highestSeatId)
+  var mySeatId: Int = -1
+  allSeatsIds.forEach {
+    if(!occupiedSeatIds.contains(it) && occupiedSeatIds.contains(it+1) && occupiedSeatIds.contains(it-1)) {
+      mySeatId = it
+    }
+  }
+  println(mySeatId)
 }
